d75e8e55ee892ca81130b40a647392c98f6de8dc
White Devil <64742147+Hacksmith64@users.noreply.github.com>
2022-03-15 21:39:59 +0530

    Arithmetic Operator

---
+++ arithmetic_operator.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int a = 10;
+int b = 13;
+
+int main()
+{
+    printf("%d + %d = %d\n", a, b, a+b);
+    printf("%d - %d = %d\n", a, b, a-b);
+    printf("%d * %d = %d\n", a, b, a*b);
+    printf("%d / %d = %d\n", a, b, a/b);
+    printf("%d  %d = %d\n", a, b, a%b);
+}
